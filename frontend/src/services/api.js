f4f2a83361933a569f513d4687d6ac6218f548fd
Raz3-33 <raziq@ocelots.in>
2025-07-29 15:00:06 +0530

    base url changed

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export const api = axios.create({
   // baseURL: "http://localhost:5000/api",
-  baseURL: import.meta.env.VITE_API_BASE_URL || "https://100.27.216.198/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "http://100.27.216.198/api",
 });
